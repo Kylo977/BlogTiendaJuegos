707599413e96ea5977c8b7df41a064c08b83e65c
Kylo977 <76027865+Kylo977@users.noreply.github.com>
2020-12-14 23:16:08 -0600

    Add files via upload

---
+++ Nuevos.blade.php
@@ -0,0 +1,95 @@
+</!DOCTYPE html>
+<html>
+<head>
+	<title>La Tienda del Gamer</title>
+    <link rel="stylesheet" href="public/Mejoras.css">
+	<STYLE type="text/css">
+  		H1 { text-align: center}
+  	</STYLE>
+  	<link rel="shortcut icon" type="image/x-icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/1200px-Xbox_one_logo.svg.png">
+</head>
+<body style="background-color:#454545;">
+	<br />
+	<h1>La Tienda del Gamer</h1>
+	<br />
+	<p align="center">
+		<font size=4 face="Comic Sans MS,arial">
+        Estos son juegos que saldrán en el 2021
+    	</font>
+	</p>
+    <p>
+        <font size=4 face="Comic Sans MS,arial">
+            Lanzamientos de enero de 2021
+            <br />
+            <br />
+        </font>
+    </p>
+    <div><img src="https://d2skuhm0vrry40.cloudfront.net/2020/articles/2020-10-02-15-30/Enero.png/EG11/resize/690x-1/quality/75/format/jpg" width="520" height="270"></div>
+    <br />
+    <br />
+    <p>
+        <font size=4 face="Comic Sans MS,arial">
+            Lanzamientos de enero de 2021
+            <br />
+            <ul>
+                <li>Hitman III (PC, PS4, PS5, Xbox One, Xbox Series X) - 20 de enero</li>
+                <li>Prince of Persia: Las Arenas del Tiempo Remake (PC, PS4, Xbox One) - 21 de enero</li>
+                <li>Ride 4 (PS5, XSX) - 21 de enero</li>
+            </ul>
+        </font>
+    </p>
+    <br />
+    <br />
+    <p>
+        <font size=4 face="Comic Sans MS,arial">
+            Lanzamientos de febrero de 2021
+            <br />
+            <br />
+        </font>
+    </p>
+    <div><img src="https://d2skuhm0vrry40.cloudfront.net/2020/articles/2020-11-13-14-50/NSwitch_BravelyDefaultII_01.jpg/EG11/resize/690x-1/quality/75/format/jpg" width="520" height="270"></div>
+    <br />
+    <br />
+    <p>
+        <font size=4 face="Comic Sans MS,arial">
+            Lanzamientos de febrero de 2021
+            <br />
+            <ul>
+                <li>Outriders (PC, PS4, PS4, Xbox One, Xbox Series X) - 2 de febrero</li>
+                <li>Werewolf: The Apocalypse - Earthblood (PC, PS4, PS5, Xbox One, Xbox Series X) - 4 de febrero</li>
+                <li>Nioh Collection (PS5) - 5 de febrero</li>
+                <li>Nioh 2: The Complete Edition (PC) - 5 de febrero</li>
+                <li>Ys IX: Monstrum Nox (PC, Switch) - 5 de febrero</li>
+                <li>Little Nightmares II (PC, PS4, Switch, Xbox One) - 11 de febrero</li>
+                <li>Super Mario 3D World + Bowser's Fury (Switch) - 12 de febrero</li>
+                <li>Riders Republic (PC, PS4, PS5, Xbox One, Xbox Series X, Stadia) - 25 de febrero</li>
+                <li>Bravely Default 2 (Switch) - 26 de febrero</li>
+                <li>Destruction AllStars (PS5) - Febrero</li>
+            </ul>
+        </font>
+    </p>
+    <p>
+        <font size=4 face="Comic Sans MS,arial">
+            Lanzamientos sin fecha en el 2021
+            <br />
+            <br />
+        </font>
+    </p>
+    <div><img src="https://d2skuhm0vrry40.cloudfront.net/2020/articles/2020-10-02-15-30/2021generico.jpg/EG11/resize/690x-1/quality/75/format/jpg" width="520" height="270"></div>
+    <p>
+        <font size=4 face="Comic Sans MS,arial">
+            Lanzamientos sin fecha en el 2021
+            <br />
+            <ul>
+                <li>Halo Infinite (PC, Xbox One, Xbox Series X) - 2021</li>
+                <li>Hogwarts Legacy (PC, PS4, PS5, Xbox One, XSX) - 2021</li>
+                <li>It Takes Two - 2021</li>
+                <li>Far Cry 6 (PC, PS4, PS5, Xbox One, Xbox Series X, Stadia) - 2021</li>
+                <li>Everspace 2 (PC, consolas) - 2021</li>
+                <li>Darkest Dungeon 2 (PC) - 2021</li>
+                <li>Control: Ultimate Edition (PS5, Xbox Series X) - 2021</li>
+                <li>Arkham Horror: Mother's Embrace (PC, PS4, Switch, Xbox One) - 2021</li>
+                <li>Vampire: The Masquerade - Bloodlines 2 (PC, PS4, Xbox One, Xbox Series X) - 2021</li>
+            </ul>
+        </font>
+    </p>
